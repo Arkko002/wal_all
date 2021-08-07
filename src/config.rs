acdf73ef17caac2d690152ad51632011a20e24db
Arkadiusz Kołodziejek <arkko002@gmail.com>
2021-08-07 21:38:38 +0200

    Config uses lists now, basic error handling with logging

--- src/config.rs
+++ src/config.rs
@@ -1,33 +1,44 @@
-use std::{env, path::{Path, PathBuf}, str::FromStr};
-use serde_derive::{Serialize, Deserialize};
+use serde_derive::{Deserialize, Serialize};
+use std::{collections::HashMap, env, path::PathBuf, str::FromStr};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Config {
-    pub alacritty_bin: PathBuf,
-    pub discord_bin: PathBuf,
-    pub discord_config: PathBuf,
-    pub pywalfox_bin: PathBuf,
-    pub zathura_bin: PathBuf,
-    pub zathura_config: PathBuf,
+pub struct WalAllConfig {
+    pub exec_list: Vec<PathBuf>,
+    pub exec_with_args: HashMap<PathBuf, Vec<String>>,
+    pub exec_with_write: HashMap<PathBuf, PathBuf>,
 }
 
-impl Default for Config {
+impl Default for WalAllConfig {
     fn default() -> Self {
+        let vec_exec = vec![
+            find_in_path("walcritty.sh").unwrap(),
+            find_in_path("pywal-discord").unwrap(),
+        ];
+
+        let mut map_with_args = HashMap::new();
+        map_with_args.insert(
+            find_in_path("pywalfox").unwrap(),
+            vec!["update".to_string()],
+        );
+
+        let mut map_with_write = HashMap::new();
+        map_with_write.insert(
+            find_in_path("genzathurarc").unwrap(),
+            PathBuf::from_str("/home/arkko/.config/zathura/zathurarc").unwrap(),
+        );
+
         Self {
-            alacritty_bin: find_in_path("alacritty").unwrap(),
-            discord_bin: find_in_path("pywal-discord").unwrap(),
-            pywalfox_bin: find_in_path("pywalfox").unwrap(),
-            zathura_bin: find_in_path("genzathura").unwrap(),
-            zathura_config: PathBuf::from_str("~/.config/zathura/zathurarc").unwrap(),
-            discord_config: PathBuf::from_str("~/.config/BetterDiscord/themes/pywal-discord-default.theme.css").unwrap()
+            exec_list: vec_exec,
+            exec_with_args: map_with_args,
+            exec_with_write: map_with_write,
         }
     }
 }
 
-fn find_in_path<P>(exe: P) -> Option<PathBuf> where P: AsRef<Path>, {
+fn find_in_path(exe: &str) -> Option<PathBuf> {
     env::var_os("PATH").and_then(|paths| {
         env::split_paths(&paths).find_map(|dir| {
-            let full_path = dir.join(&exe);
+            let full_path = dir.join(exe);
             if full_path.is_file() {
                 Some(full_path)
             } else {
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,90 @@
-use std::{fs::File, io::Error, io::Write, path::PathBuf, process::Command};
+use std::{
+    fs::{File, OpenOptions},
+    io::Error,
+    io::Write,
+    path::PathBuf,
+    process::{Command, ExitStatus},
+};
+
+use chrono::Local;
 pub mod config;
 
 fn main() -> Result<(), Error> {
-    let cfg: config::Config = confy::load("config").unwrap();
+    let cfg: config::WalAllConfig = confy::load("wal_all").unwrap();
 
-    exec_cmd(cfg.alacritty_bin).unwrap();
-    exec_cmd_with_args(cfg.pywalfox_bin, vec!["update".to_string()]).unwrap();
-    exec_cmd_with_write(cfg.discord_bin, cfg.discord_config).unwrap();
-    exec_cmd_with_write(cfg.zathura_bin, cfg.zathura_config)
-}
-
-fn exec_cmd(cmd: PathBuf) -> Result<(), Error> {
-    let mut cmd = Command::new(cmd);
-    match cmd.status() {
-        Ok(_) => Ok(()),
-        Err(err) => Err(err),
+    for cmd in cfg.exec_list.iter() {
+        match exec_cmd(cmd) {
+            Ok(status) => match status.success() {
+                true => continue,
+                false => {
+                    let err_msg = format!("Error at {} execution.", cmd.to_str().unwrap());
+                    write_error_log(err_msg)
+                }
+            },
+            Err(err) => write_error_log(err.to_string()),
+        }
     }
+
+    for cmd in cfg.exec_with_args.iter() {
+        match exec_cmd_with_args(cmd.0, cmd.1) {
+            Ok(status) => match status.success() {
+                true => continue,
+                false => {
+                    // TODO
+                    let err_msg = format!("Error at {} execution.", cmd.0.to_str().unwrap());
+                    write_error_log(err_msg)
+                }
+            },
+            Err(err) => write_error_log(err.to_string()),
+        }
+    }
+
+    for cmd in cfg.exec_with_write.iter() {
+        match exec_cmd_with_write(cmd.0, cmd.1) {
+            Ok(status) => match status.success() {
+                true => continue,
+                false => {
+                    let err_msg = format!("Error at {} execution.", cmd.0.to_str().unwrap());
+                    write_error_log(err_msg)
+                }
+            },
+            Err(err) => write_error_log(err.to_string()),
+        }
+    }
+
+    Ok(())
 }
 
-fn exec_cmd_with_args(cmd: PathBuf, args: Vec<String>) -> Result<(), Error> {
+fn exec_cmd(cmd: &PathBuf) -> Result<ExitStatus, Error> {
+    let mut cmd = Command::new(cmd);
+    Ok(cmd.status()?)
+}
+
+fn exec_cmd_with_args(cmd: &PathBuf, args: &Vec<String>) -> Result<ExitStatus, Error> {
     let mut cmd = Command::new(cmd);
     cmd.args(args);
-    match cmd.status() {
-        Ok(_) => Ok(()),
-        Err(err) => Err(err)
-    }
+    Ok(cmd.status()?)
 }
 
-fn exec_cmd_with_write(cmd: PathBuf, cfg_file: PathBuf) -> Result<(), Error> {
+fn exec_cmd_with_write(cmd: &PathBuf, cfg_file: &PathBuf) -> Result<ExitStatus, Error> {
     let mut cmd = Command::new(cmd);
-    let scheme = cmd.output().unwrap();
+    let scheme = cmd.output()?;
 
-    let mut file = File::create(cfg_file).unwrap();
-    file.write_all(&scheme.stdout)
+    let mut file = File::create(cfg_file)?;
+    file.write_all(&scheme.stdout)?;
+
+    Ok(scheme.status)
+}
+
+fn write_error_log(err: String) {
+    let mut logs = OpenOptions::new()
+        .append(true)
+        .open("./wal_all.logs")
+        .unwrap();
+
+    let datetime = Local::now();
+    let header = format!("\n\n======== {} ========", datetime.naive_utc().to_string());
+
+    logs.write(header.as_bytes()).unwrap();
+    logs.write(err.as_bytes()).unwrap();
 }
